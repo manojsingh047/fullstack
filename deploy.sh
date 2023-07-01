f89532951dda2beb4b7cab683f641179c2217e46
manoj <manojsingh047@gmail.com>
2023-07-01 18:56:21 +0530

    updated

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sshcmd="ssh -i /home/manojs/.ssh/id_rsa manojs@35.208.175.52"
-$sshcmd screen -d -m /home/manojs/fullstack prod_deploy.sh
+sshcmd="ssh -t -i /home/manojs/.ssh/id_rsa manojs@35.208.175.52"
+$sshcmd screen -S "deployment" sh /home/manojs/fullstack prod_deploy.sh
